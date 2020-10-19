a991778c4360d01bc32ad530b404d81db9d9e541
SimranArora123 <52946773+SimranArora123@users.noreply.github.com>
2020-10-19 10:25:38 +0530

    Tribute page on stephen hawkings

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs =require("ejs");
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/main.html");
+});
+app.get("/second", function(req, res){
+  res.render("second");
+});
+
+
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function(){
+    console.log("Server  started on port 3000 ");
+});
